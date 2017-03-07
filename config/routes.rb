3d32d76ea08c729c7e15473bd32d31e9b9f3ac61
nedim <nedim@ned.local>
2017-03-07 18:03:48 +0100

    add callback to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'user#index'
   resources :user
 end
