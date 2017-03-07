2c312f8a4700543d1b15fd4db2e1a25d647c5c6e
Nedim Hadzimahmutovic <nedim@Nedims-Mac-Pro.local>
2017-03-07 11:18:51 +0100

    omniauth_providers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,3 @@
-# Use this hook to configure devise mailer, warden hooks and so forth.
-# Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -248,6 +246,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+config.omniauth :facebook, "APP_ID", "APP_SECRET"
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
