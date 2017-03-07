0f4080ce59f336007ffee83769780e22149419ca
nedim <nedim@ned.local>
2017-03-07 20:38:45 +0100

    users json export

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,13 +4,13 @@ class UserController < ApplicationController
     @users = User.all
   end
 
-  def json
-    data = User.all.to_json
-    send_data data, :type => 'application/json; header=present', :disposition => "attachment; filename=users.json"
-  end
-
   def show
     @user = User.find(params[:id])
+  end
+
+  def json
+    @users =  User.all
+    render json: JSON.pretty_generate(@users.as_json)
   end
 
 end
