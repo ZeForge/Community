b18b897e39f5eb5f65a3bdad366e9101d99dc7e3
nedim <nedim@ned.local>
2017-03-09 18:29:57 +0100

    fix users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root to: 'user#index'
-  resources :user
-  get 'users/json' => 'user#json'
+  root to: 'users#index'
+  resources :users
+  get 'users/json' => 'users#json'
 end
