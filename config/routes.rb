f515289c7a3f51179c0c11e2ca91c36d46218af2
nedim <nedim@ned.local>
2017-03-11 21:44:25 +0100

    izmjene

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'dashboards#show'
+  devise_for :admins, controllers: { sessions: 'admin/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
+  #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users
   resources :posts
   get 'users/json' => 'users#json'
