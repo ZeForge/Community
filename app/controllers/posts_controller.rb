4a48e0e93ac35638818d3bbfc6d00f0b12c0184e
nedim <nedim@ned.local>
2017-03-11 20:37:33 +0100

    post validacije

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to root_url
+    if @post.save
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   private
