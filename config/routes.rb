117b8017bee7d4b1edf124b853fc89f408bb457b
Nedim Hadzimahmutovic <nedim@Nedims-Mac-Pro.local>
2017-03-16 14:23:48 +0100

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
   #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  resources :users
+  
   resources :users, only: [:edit, :update] do
     resources :recommended_posts, only: [:index]
   end
