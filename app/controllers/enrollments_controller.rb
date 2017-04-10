28cfca7cf6905018a8f832661b6c5044fb99158c
Nedim Hadzimahmutovic <nedim@Nedims-Mac-Pro.local>
2017-04-10 09:34:10 +0200

    add create methot for course enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,2 +1,10 @@
 class EnrollmentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    course = Course.find params[:course_id]
+
+    Enrollment.create user_id: current_user.id, course_id: course.id
+    redirect_to course_path(course)
+  end
 end
