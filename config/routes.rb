930f3be2012e02feda03b01d11dc454935da2fb4
neidiom <h.nedim@gmail.com>
2017-04-24 13:22:05 +0200

    disable sign up and forget password

--- config/routes.rb
+++ config/routes.rb
@@ -131,7 +131,7 @@ Rails.application.routes.draw do
   # Devise
   #
   devise_for :admins, controllers: { sessions: 'admin/sessions' }
-  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
   #
   #
   # devise_scope :user do
